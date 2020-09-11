b8acdf85c2d79be30935a58db69e417d67a12cb0
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-09-11 08:11:22 -0700

    update stater code

--- my_code.py
+++ my_code.py
@@ -1,16 +1,19 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-def inc(x):
-    # User code goes here
-    return x + 1
+def guess_number(guess):
+
+    # Use these return statements in your function, depending on the situation. Do not change the text, but feel free to move them around.
+    return "Your number is too high."
+    return "Your number is too low."
+    return "Your number is out of range."
+    return "I'm sorry you are giving up!"
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(inc(6))
+    x = int(input("Gess a number: "))
+    
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+
